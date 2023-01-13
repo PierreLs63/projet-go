335fbd5b6b874eef8ee77acc11bd7acac3b248c0
PierreLs63 <pierrelouis.vidal@hotmail.fr>
2023-01-13 16:33:35 +0100

    init go routine

--- main.go
+++ main.go
@@ -6,37 +6,43 @@ import (
 	"time"
 )
 
-var wg sync.WaitGroup // instanciation de notre structure WaitGroup
-const N int = 3
-
-var a = [N][N]int{
-	{0, 1, 2},
-	{3, 4, 5},
-	{6, 7, 8},
-}
-var b = [N][N]int{
+var a = [][]uint8{
 	{0, 1, 2},
 	{3, 4, 5},
 	{6, 7, 8},
 }
 
-func run(i int, j int) {
-	defer wg.Done()
-	for i := 0; i < N; i++ {
-
-	}
+var b = [][]uint8{
+	{0, 1, 2},
+	{3, 4, 5},
+	{6, 7, 8},
 }
 
 func main() {
-	debut := time.Now()
-	for i := 0; i < N; i++ {
-		for j := 0; j < N; i++ {
+	var wg sync.WaitGroup //  initialise a counter             // one go routine to wait for
+	canal := make(chan uint8)
+	start := time.Now()
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a); j++ {
 			wg.Add(1)
-			go run(i, j)
+			go func(i int, j int) {
+				defer wg.Done()
+				cumputeCase(canal, a, b, i, j)
+			}(i, j)
 		}
 	}
+	for l := 0; l < len(a)*len(a); l++ {
+		//fmt.Println(<-canal)
+		<-canal
+	}
+	fmt.Println(time.Since(start))
+	wg.Wait() // block until all routines are done executing
+}
 
-	wg.Wait()
-	fin := time.Now()
-	fmt.Println(fin.Sub(debut))
+func cumputeCase(c chan uint8, m1 [][]uint8, m2 [][]uint8, i int, j int) {
+	var res uint8 = 0
+	for k := 0; k < len(m1[0]); k++ {
+		res += m1[i][k] * m2[k][j]
+	}
+	c <- res
 }
