b9d2a4420652d2ae106a855f5e3e851e728bf82d
PierreLs63 <pierrelouis.vidal@hotmail.fr>
2023-01-19 09:41:23 +0100

    mise en place du channel entre cumptecase et main

--- main.go
+++ main.go
@@ -6,23 +6,23 @@ import (
 	"time"
 )
 
-var a = [][]uint8{
+var a = [][]int{
 	{0, 1, 2},
 	{3, 4, 5},
 	{6, 7, 8},
 }
 
-var b = [][]uint8{
+var b = [][]int{
 	{0, 1, 2},
 	{3, 4, 5},
 	{6, 7, 8},
 }
 
 func main() {
-	var wg sync.WaitGroup //  initialise a counter             // one go routine to wait for
-	canal := make(chan uint8)
+	var wg sync.WaitGroup //  initialise a counter
+	canal := make(chan [3]int, 2)
 	start := time.Now()
-	for i := 0; i < len(a); i++ { //ir huyhjg vkiyf
+	for i := 0; i < len(a); i++ {
 		for j := 0; j < len(a); j++ {
 			wg.Add(1)
 			go func(i int, j int) {
@@ -32,17 +32,20 @@ func main() {
 		}
 	}
 	for l := 0; l < len(a)*len(a); l++ {
-		//fmt.Println(<-canal)
-		<-canal
+		res := <-canal
+		fmt.Println(res)
 	}
 	fmt.Println(time.Since(start))
 	wg.Wait() // block until all routines are done executing
 }
 
-func cumputeCase(c chan uint8, m1 [][]uint8, m2 [][]uint8, i int, j int) {
-	var res uint8 = 0
+func cumputeCase(c chan [3]int, m1 [][]int, m2 [][]int, i int, j int) {
+	var res [3]int
+	res[0] = 0
 	for k := 0; k < len(m1[0]); k++ {
-		res += m1[i][k] * m2[k][j]
+		res[0] += m1[i][k] * m2[k][j]
 	}
+	res[1] = i
+	res[2] = j
 	c <- res
 }
